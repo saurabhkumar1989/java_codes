2ac328f12ece566bf07da7a0e977d69bd7662865
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-05-25 21:01:45 -0400

    d

--- src/java_code/Test.java
+++ src/java_code/Test.java
@@ -4,7 +4,20 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char ch = 's';// however "" is not good
+		String name = "Name"; 
+		if (ch=='s') {
+			System.out.print("test");
+		
+		} else {
+			// todo
+		}
+		int number = 1;
+		int sum = 100;
+		for (int i = 0; i < sum; i++) {
+			number = number + 1;			
+		}
+		System.out.println(number);
 	}
 
 }
