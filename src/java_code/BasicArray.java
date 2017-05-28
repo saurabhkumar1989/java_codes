0b78db1fdbe58301a62bffb7d70513cc7b08d75f
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-05-27 21:28:41 -0400

    s

---
+++ src/java_code/BasicArray.java
@@ -0,0 +1,18 @@
+package java_code;
+
+import java.util.ArrayList;
+
+public class BasicArray {
+	public static void main(String [] args) {
+		double [] rainfall = new double[365];// array of size 365
+		// array is fixed size - for dynamic type use list
+		ArrayList<String> names = new ArrayList<String>();
+		// list of sting type :)
+		names.add("firt element");
+		names.add("second element");
+		for (String item : names) {// data type list return variable : list
+			System.out.println(item);
+		}
+		System.out.println(names.size());
+	}
+}
